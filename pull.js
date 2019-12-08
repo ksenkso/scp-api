0017524be8bb75d3f3997d64a5f71639ab8e8e65
ksenkso <yazunya@gmail.com>
2019-12-08 23:19:14 +0300

    Initial commit

---
+++ pull.js
@@ -0,0 +1,117 @@
+import request from 'request-promise-native';
+import jsdom from 'jsdom';
+
+const links = [
+    'http://scpfoundation.net/scp-list',
+    'http://scpfoundation.net/scp-list-2',
+    'http://scpfoundation.net/scp-list-3',
+    'http://scpfoundation.net/scp-list-4',
+    'http://scpfoundation.net/scp-list-5',
+];
+
+export async function pull(connection) {
+    const tasks = links.map(async link => {
+        console.log('Starting link', link, '...');
+        const selector = ['keter', 'euclid', 'safe', 'na', 'thaumiel', 'nonstandard',]
+            .map(type => `img[alt^="${type}"]`)
+            .join(',');
+
+        const html = await request(link);
+        console.log('Page loaded.');
+        const dom = new jsdom.JSDOM(html);
+        const objects = Array.from(dom
+            .window
+            .document
+            .querySelectorAll(selector))
+            .slice(6)
+            .map(image => {
+                return {
+                    number: getNumber(image),
+                    name: getName(image),
+                    link: getLink(image),
+                    class: getClass(image),
+                };
+            });
+        console.log('Inserting to DB...');
+        return loadObjects(connection, objects).then(() => console.log(`${link} done!`));
+    });
+    return Promise.all(tasks);
+}
+
+/**
+ *
+ * @param {Connection} connection
+ * @param {ScpObject[]} objects
+ * @return {Promise<void>}
+ */
+async function loadObjects(connection, objects) {
+    let template = [];
+    let values = [];
+    objects.forEach(object => {
+        template.push(`(?, ?, ?, ?)`);
+        values.push(object.name, object.number, object.link, object.class);
+    });
+    connection.query({
+        sql: 'insert into objects (name, number, link, class) values ' + template.join(','),
+        values
+    });
+}
+
+function getAnchor(image) {
+    return image.nextElementSibling.tagName === 'A'
+        ? image.nextElementSibling
+        : image.nextElementSibling.querySelector('a');
+}
+
+function getNumber(image) {
+    const link = getAnchor(image);
+    if (!link) {
+        return null;
+    }
+    let match = link.textContent.match(/(\d+)/);
+    if (match) {
+        return +match[1];
+    } else {
+        match = link.href.match(/scp-(\d+)/);
+        if (match) {
+            return +match[1];
+        } else {
+            return null;
+        }
+    }
+}
+
+function getName(image) {
+    const link = getAnchor(image);
+    if (link === null) {
+        return null;
+    }
+    const content = link.nextSibling ? link.nextSibling.textContent : link.textContent;
+    if (content.startsWith(' — ')) {
+        return content.slice(3)
+    } else {
+        return content;
+    }
+}
+
+function getLink(image) {
+    const link = getAnchor(image);
+    if (link === null) {
+        return null;
+    }
+    if (link.href.match(/\/scp-\d+/)) {
+        return 'http://scpfoundation.net' + link.href;
+    } else {
+        const number = getNumber(image);
+        if (number !== null) {
+            return 'http://scpfoundation.net/scp-' + number.toString().padStart(3, '0');
+        } else {
+            return null;
+        }
+    }
+}
+
+function getClass(image) {
+    return image.alt.match(/keter|euclid|safe|na|thaumiel|nonstandard/)[0];
+}
+
---
+++ server.js
@@ -0,0 +1,174 @@
+/**
+ *
+ * @typedef {{id?: number, name: string, number: number, link: string, class: string}} ScpObject
+ */
+import {pull} from './pull.js';
+import Fastify from 'fastify';
+import fastifyMysql from 'fastify-mysql';
+import cors from 'fastify-cors';
+
+const fastify = Fastify({logger: true});
+
+fastify.register(fastifyMysql, {
+    promise: true,
+    connectionString: 'mysql://root:root@localhost/scp'
+});
+
+fastify.register(cors, {
+    origin: true,
+});
+
+const OBJECT_DATA = {
+    type: 'object',
+    properties: {
+        name: {type: 'string'},
+        number: {type: 'integer'},
+        link: {type: 'string'},
+        class: {type: 'string'},
+    }
+};
+
+const CREATED_OBJECT = {
+    type: 'object',
+    properties: {
+        id: {type: 'integer'},
+        name: {type: 'string'},
+        number: {type: 'integer'},
+        link: {type: 'string'},
+        class: {type: 'string'},
+    }
+};
+const types = {OBJECT_DATA, CREATED_OBJECT};
+
+async function getObject(connection, id) {
+    console.log(id);
+    const [rows] = await connection.query({
+        sql: 'select * from objects where id = ?',
+        values: [id]
+    });
+    return rows[0] || null;
+}
+fastify.route({
+    url: '/objects',
+    method: 'GET',
+    schema: {
+        response: {
+            200: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        id: {type: 'integer'},
+                        name: {type: 'string'},
+                        number: {type: 'integer'},
+                        link: {type: 'string'},
+                        class: {type: 'string'},
+                    }
+                }
+            }
+        }
+    },
+    handler: async (req, res) => {
+        const connection = await fastify.mysql.getConnection();
+        const [rows] = await connection.query('select * from objects');
+        return rows;
+    }
+});
+
+fastify.route({
+    url: '/objects',
+    method: 'POST',
+    schema: {
+        body: types.OBJECT_DATA,
+        response: {
+            201: {
+                type: 'integer'
+            }
+        }
+    },
+    handler: async (req, res) => {
+        const connection = await fastify.mysql.getConnection();
+        const [rows] = await connection.query({
+            sql: 'insert into objects (name, number, link) value (?, ?, ?)',
+            values: [req.body.name, req.body.number, req.body.link]
+        });
+        res.status(201).send(rows.insertId);
+    }
+});
+
+fastify.route({
+    url: '/objects/:id',
+    method: 'PATCH',
+    schema: {
+        body: types.OBJECT_DATA,
+        response: {
+            200: types.CREATED_OBJECT
+        }
+    },
+    handler: async (req, res) => {
+        const connection = await fastify.mysql.getConnection();
+        const keys = ['name', 'number', 'link', 'class'].filter(k => req.body[k] !== undefined);
+        const values = keys.map(k => req.body[k]);
+        const [rows] = await connection.query({
+            sql: `update objects set ${keys.map(k => `${k} = ?`).join(',')} where id = ?`,
+            values: [...values, req.params.id]
+        });
+        return await getObject(connection, req.params.id);
+    }
+});
+
+fastify.route({
+    url: '/objects/:number',
+    method: 'GET',
+    handler: async (req, res) => {
+        const connection = await fastify.mysql.getConnection();
+        const [rows] = await connection.query({
+            sql: 'select * from objects where number = ?',
+            values: [req.params.number]
+        });
+        return rows || null;
+    }
+});
+
+fastify.route({
+    url: '/objects/byId/:id',
+    method: 'GET',
+    handler: async (req, res) => {
+        const connection = await fastify.mysql.getConnection();
+        return await getObject(connection, req.params.id);
+    }
+});
+
+fastify.route({
+    url: '/objects/:id',
+    method: 'DELETE',
+    handler: async (req, res) => {
+        const connection = await fastify.mysql.getConnection();
+        await connection.query({
+            sql: 'delete from objects where id = ?',
+            values: [req.params.id]
+        });
+        return req.params.id;
+    }
+});
+
+fastify.route({
+    url: '/pull',
+    method: 'GET',
+    handler: async (req, res) => {
+        return pull(await fastify.mysql.getConnection())
+            .then(() => res.status(200).send('OK'));
+    }
+});
+
+const start = async () => {
+    try {
+        await fastify.listen(3000);
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    } catch (e) {
+        fastify.log.error(e);
+        process.exit(1);
+    }
+};
+
+start();
