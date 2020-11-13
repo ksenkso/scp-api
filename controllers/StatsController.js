e8c824f43f67efb639a24bcc2d3aa5572cb50873
ksenkso <yazunya@gmail.com>
2020-11-13 20:34:15 +0300

    feat(pull): use lastPull value from DB for stats

--- controllers/StatsController.js
+++ controllers/StatsController.js
@@ -2,15 +2,15 @@ import fastify from '../server.js';
 
 /**
  *
- * @return {Promise<{total: number, lastModified: number, last: number}>}
+ * @return {Promise<{total: number, lastPull: number, last: number}>}
  */
 export const getStats = async () => {
     const connection = await fastify.mysql.getConnection();
     const [rows] = await connection.query('select count(id) as `total`, max(cast((substring(link, 30)) as unsigned)) as `last` from objects');
-    console.log(rows[0]);
+    const [lastPull] = await connection.query('select value as lastPull from stats where name = \'lastPull\'');
     return {
         total: rows[0].total,
         last: rows[0].last,
-        lastModified: +Date.now()
+        lastPull: lastPull[0] ? +lastPull[0].lastPull : null
     }
 };
